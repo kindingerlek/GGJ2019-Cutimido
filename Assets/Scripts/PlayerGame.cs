a992f55cf8b389fbecddc1be354ba21d19720e3b
Lucas Ernesto Kindinger <kindingerlek@hotmail.com>
2019-01-26 11:22:22 -0200

    Criado controle de personagem baseado em rigidbody

---
+++ Assets/Scripts/PlayerGame.cs
@@ -0,0 +1,98 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using Rewired;
+using Rewired.Demos;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerGame : MonoBehaviour {
+
+    public int gamePlayerId = 0;
+
+    public float speed = 10.0f;
+    public float gravity = 10.0f;
+    public float maxVelocityChange = 10.0f;
+    public float jumpHeight = 2.0f;
+    public bool canJump = true;
+
+    private bool fire;
+    private bool jump;
+    private bool grounded = false;
+
+    private Vector3 moveVector;
+
+    private new Rigidbody rigidbody;
+    private Rewired.Player player { get { return PressStartToJoinExample_Assigner.GetRewiredPlayer(gamePlayerId); } }
+
+
+    void Awake()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+
+        rigidbody.freezeRotation = true;
+        rigidbody.useGravity = false;
+    }
+
+    private void GetInput()
+    {
+        // Get the input from the Rewired Player. All controllers that the Player owns will contribute, so it doesn't matter
+        // whether the input is coming from a joystick, the keyboard, mouse, or a custom controller.
+
+        moveVector.x = player.GetAxis("Move Horizontal"); // get input by name or action id
+        moveVector.y = player.GetAxis("Move Vertical");
+        fire = player.GetButtonDown("Fire");
+        jump = player.GetButtonDown("Jump");
+    }
+
+    void Update()
+    {
+        if (!ReInput.isReady) return; // Exit if Rewired isn't ready. This would only happen during a script recompile in the editor.
+        if (player == null) return;
+
+        GetInput();
+    }
+
+    void FixedUpdate()
+    {
+        if (grounded)
+        {
+            // Calculate how fast we should be moving
+            Vector3 targetVelocity = new Vector3(moveVector.x, 0, moveVector.y);
+            targetVelocity = transform.TransformDirection(targetVelocity);
+            targetVelocity *= speed;
+
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = rigidbody.velocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+            rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+
+            // Jump
+            if (canJump && jump)
+            {
+                rigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+            }
+        }
+
+
+
+        // We apply gravity manually for more tuning control
+        rigidbody.AddForce(new Vector3(0, -gravity * rigidbody.mass, 0));
+
+        grounded = false;
+    }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    float CalculateJumpVerticalSpeed()
+    {
+        // From the jump height and gravity we deduce the upwards speed 
+        // for the character to reach at the apex.
+        return Mathf.Sqrt(2 * jumpHeight * gravity);
+    }
+}
