76134db2df518083d58a68b7ed049b567aaf8930
misael santos <misael.300@hotmail.com>
2019-01-27 11:31:35 -0200

    Tela creditos finalizada

--- Assets/Scripts/ViewController/ChangeScene.cs
+++ Assets/Scripts/ViewController/ChangeScene.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
+public class ChangeScene :MonoBehaviour
 {
-    public void LoadScene(string nameScene) {
-        Application.LoadLevel(nameScene);
+    public void LoadScene(int sceneId)
+    {
+
+        SceneManager.LoadScene(sceneId);
     }
 }
---
+++ Assets/Scripts/ViewController/Credits.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Credits: MonoBehaviour
+{
+    private float startTime;
+    public int status;
+    ChangeScene Cs;
+    public void LoadCredits() {
+        if (status == (int)EnumCredits.Designer)
+        {
+            Cs.LoadScene(7);
+            return;
+        }
+        else if (status == (int) EnumCredits.Artist)
+        {
+            Cs.LoadScene(1);
+            return;
+        }
+        else if (status == (int)EnumCredits.Programmer)
+        {
+            Cs.LoadScene(3);
+            return;
+        }
+        else if (status == (int)EnumCredits.Music)
+        {
+            return;
+        }
+        else if (status == 5)
+        {
+            Cs.LoadScene(2);
+            return;
+        }
+
+
+    }
+
+    void Start()
+    {
+        startTime = Time.time;
+        Cs = new ChangeScene();
+
+    }
+
+    private void Update()
+    {
+        if (Time.time - startTime  > 3)
+        {
+            LoadCredits();
+        }
+    }
+}
